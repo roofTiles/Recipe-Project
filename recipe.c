1dea9e5b91787c84e1e24e3240fc4b85353a409d
roofTiles <guhatejas@gmail.com>
2021-03-08 12:38:43 -0500

    basic skeleton
    input/output (menus) and basic program flow

---
+++ recipe.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "recipe.h"
+
+
+FILE *database;
+
+void main(){
+
+  /* Get valid database and read it */
+  int valid_database = FALSE;
+  while (!valid_database){
+    valid_database = get_valid_database();
+  }
+
+  /* Parse Database */
+
+
+  /* Get valid function option and perform it */
+  int valid_option;
+  while (TRUE){
+    valid_option = FALSE;
+    printf("1.  Print summary\n2.  Print summary by prep time\n3.  Print summary by category\n4.  Print recipe\n5.  Exit\n");
+
+    while(!valid_option){
+      valid_option = get_valid_option();
+    }
+  }
+}
+
+
+
+/* Gets input for database and opens it if valid */
+int get_valid_database(){
+  int input;
+  printf("Enter database (0, 1, 2):  ");
+  scanf("%d", &input);
+  switch(input){
+      case 0:
+	database = fopen(FIRST_DATABASE, "r");
+	break;
+
+      case 1:
+	database = fopen(SECOND_DATABASE, "r");
+	break;
+
+      case 2:
+	database = fopen(THIRD_DATABASE, "r");
+	break;
+
+      default:
+	invalid_input();
+	return(FALSE);
+  }
+
+  return(TRUE);
+}
+
+/* Gets input for action and performs it if valid */
+int get_valid_option(){
+  int input;
+  printf("Enter option:  ");
+  scanf("%d", &input);
+  switch(input){
+    case 1:
+      // print_all()
+      break;
+      
+    case 2:
+      // print_prep()
+      break;
+
+    case 3:
+      // print_category()
+      break;
+
+    case 4:
+      // print_recipe()
+      break;
+
+    case 5:
+      exit(1);
+
+    default:
+      invalid_input();
+      return(FALSE);
+  }
+
+  return(TRUE);
+}
+
+
+/* Outputs invalid input if given */
+void invalid_input(){
+  printf("Invalid input\n");
+}
---
+++ recipe.h
@@ -0,0 +1,56 @@
+/* ENEE 150 Project 2 (Recipe Project) Header File */
+
+
+/* Boolean Constants */
+#define TRUE 1
+#define FALSE 0
+
+/* Size Constants */
+#define NUM_RECIPES 10000
+#define CHAR_SIZE 100
+#define CATEGORIES_PER_RECIPE 8
+#define CATEGORIES_SIZE 500
+
+/* Output Constants */
+#define ID 3;
+#define NAME 15;
+#define AUTHOR 15;
+#define PREP_TIME 4;
+#define CATEGORY_DESCRIPTORS 30;
+
+/* Database files */
+#define FIRST_DATABASE "recipes0.data"
+#define SECOND_DATABASE "recipes1.data"
+#define THIRD_DATABASE "recipes2.data"
+
+/* Recipe Data Structures */
+
+typedef struct Recipes{
+  char title[CHAR_SIZE], author[CHAR_SIZE], categories[CATEGORIES_PER_RECIPE][CHAR_SIZE];
+  char *ingredient, *instructions;
+  int prep_time[2];
+} Recipe;
+
+extern Recipe recipes[NUM_RECIPES];
+extern char all_categories[CATEGORIES_SIZE][CHAR_SIZE];
+
+
+/* Handling Input */
+
+int get_valid_database();
+int get_valid_option();
+int get_valid_type(int *, int *);
+void invalid_input();
+
+
+/* Option Functionality */
+
+void print_all();
+void print_prep();
+void print_category();
+void print_summary(int);
+void print_recipe(int);
+
+/* Parsing Database */
+void parseDatabase();
+void parseRecipe();
