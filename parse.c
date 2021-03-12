f7f141ccbec2cd6c38c81b2a62b65c276caf8ccc
roofTiles <guhatejas@gmail.com>
2021-03-12 11:28:28 -0500

    fixed category saving

--- parse.c
+++ parse.c
@@ -9,8 +9,7 @@ Recipe currentRecipe;
 void parseDatabase(){
   int numOfRecipes = 0;
   char *line, ingredients, instructions;
-  //char name[CHAR_SIZE], author[CHAR_SIZE], categories[CATEGORIES_PER_RECIPE][CHAR_SIZE];
-  
+    
   parseLine(&line);
   while (*(line) != EOF){
     if (strcmp(line, recipeHeader) == 0){
@@ -49,6 +48,8 @@ void parseRecipeHeaders(){
 
   /* Category Headers */
   int categoryIndex = 1;
+  char categories[CATEGORIES_PER_RECIPE][CHAR_SIZE];
+  
   parseLine(&line);
   sscanf(line, "Categories:  %[^\n]", currentRecipe.categories[0]);
   free(line);
@@ -59,19 +60,27 @@ void parseRecipeHeaders(){
     free(line);
     parseLine(&line);
   }
-  printf("%s\n", currentRecipe.categories[1]);
+
+  // resetting any categories not present in current recipe
+  while(categoryIndex < CATEGORIES_PER_RECIPE){
+    int c;
+    for (c = 0; c < CHAR_SIZE; c++){
+      currentRecipe.categories[categoryIndex][c] = 0;
+    }
+    categoryIndex++;
+  }
 }
 
 /* Parses a line in database */
 void parseLine(char** line){
   int size = 1;
   char c;
-  *line = (char*) malloc(size); // changing address pointer refers to
+  *line = (char*) malloc(size * sizeof(char)); // changing address pointer refers to
   do{
     c = fgetc(database);
     *(*line + size - 1) = c; // adds current characters in line to array
     size++;
-    *line = (char*) realloc(*line, size); // increases size of char array when necessary
+    *line = (char*) realloc(*line, size * sizeof(char)); // increases size of char array when necessary
   }while(c != '\n' && c != EOF);
   *(*line + size - 1) = '\0';  // add null terminator for string comparison
 }
